49e8ef6fb52501efa1e87fde9c37a87a91289c5d
mikael vinicius <mikaelvinimarques@hotmail.com>
2022-06-21 18:45:46 -0300

    Arrumado as pastas

---
+++ fundamentos/bloco-4-iniciando-javascript/exercicio1.js
@@ -0,0 +1,10 @@
+const nota = 65
+if(nota >= 80) {
+  console.log("Parabens, voce foi aprovado")
+}
+else if(nota < 80 && nota >= 60) {
+  console.log("Você está na nossa lista de espera")
+}
+else {
+  console.log("Voce foi Reprovado")
+}
