4979394a510ea915900b4b98e7ddfc459713d9a5
mikael vinicius <mikaelvinimarques@hotmail.com>
2022-06-23 19:17:26 -0300

    arrumando as pastas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio2.js
@@ -0,0 +1,39 @@
+//exercicio1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for( let number of numbers) {
+console.log(number)
+}
+//exercicio2
+let soma = 0; 
+for( let index = 0; index < numbers.length; index = index + 1) {
+  soma +=  numbers[index]     }
+console.log(soma)
+//exercicio3
+let media = 0 
+for(let index = 0; index < numbers.length; index += 1 ) {
+  media += numbers[index]
+}
+let average = media / numbers.length;
+console.log(average)
+//exercicio4
+if(average > 20) {
+  console.log("É maior que 20")
+}
+else{
+  console.log("É menor ou igual a 20")
+}
+//exercicio5
+let maior = 0
+for(let index = 0; index < numbers.length; index += 1 ) {
+ if(numbers[index] > maior) {  
+  maior = numbers[index] }
+}
+console.log(maior)
+//exercicio6
+let impares = 0
+for(let index = 0; index < numbers.length; index += 1) {
+  if(numbers[index] % 2 === 1) {
+   impares += 1 
+  }
+}
+console.log(impares)
