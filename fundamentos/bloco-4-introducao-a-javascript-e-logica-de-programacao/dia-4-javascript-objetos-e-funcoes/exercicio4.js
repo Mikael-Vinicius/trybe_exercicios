82e01c8d7eb6252329663fe694f4168285b24c4f
mikael vinicius <mikaelvinimarques@hotmail.com>
2022-06-25 19:34:55 -0300

    feito exercicios ate o 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio4.js
@@ -1,5 +1,10 @@
+//exercicio1
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
+console.log('bem vinda, ' + info.personagem)
+//exercicio2
+console.log(info.recorrente)
