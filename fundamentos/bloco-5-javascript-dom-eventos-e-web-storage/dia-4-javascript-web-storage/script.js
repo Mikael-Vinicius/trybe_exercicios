ce15893c2854bd0c80fe6bd836291d83e66b5c61
mikael vinicius <mikaelvinimarques@hotmail.com>
2022-07-01 21:11:47 -0300

    feito a base do site

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,35 @@
+let paragrafo = document.getElementById("paragraph")
+let inputColor = document.getElementById("inputColor")
+let body = document.getElementsByTagName('body')[0]
+let inputBody = document.getElementById('inputBody')
+inputColor.addEventListener('input', changeColor)
+inputBody.addEventListener('input', changeColor)
+botao = document.getElementById('button')
+botao.addEventListener('click', changeFont)
+
+function changeColor() {
+  inputColor.style.color = inputColor.value
+  paragrafo.style.color = inputColor.value
+  inputBody.style.backgroundColor = inputBody.value
+  body.style.backgroundColor = inputBody.value
+}
+
+function changeFont() {
+  paragrafo.style.fontSize = '18px'
+  paragrafo.style.letterSpacing = '1px'
+  paragrafo.style.fontFamily = 'monospace'
+}
+
+function addPhraseToLocalStorage() {
+  localStorage.setItem('paragrafo')
+  
+}
+
+
+
+function initialRenderization() {
+
+}
+
+
+window.onload = addPhraseToLocalStorage
